2fc62be31fa15b4634a42c6dd939fb613336786d
EddieToledoE <Teddy_03@outlook.es>
2025-06-01 23:00:25 -0600

    screenshot stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:no_screenshot/no_screenshot.dart';
+
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 🔐 Inicializar Firebase
   await Firebase.initializeApp();
+
+  // 🔒 Prevenir capturas desde el inicio
+  final _noScreenshot = NoScreenshot.instance;
+  await _noScreenshot.screenshotOff(); // Bloquea capturas
+
   final user = FirebaseAuth.instance.currentUser;
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
